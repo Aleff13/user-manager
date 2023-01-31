d15fa3531c4cd1dc5ce2c77ff4308108ea317ae8
Aleff13 <aleff.menna.resler@gmail.com>
2023-01-31 04:17:54 -0300

    docs: implement swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'user name',
+    minimum: 240,
+    default: '',
+  })
   name: string;
+
+  @ApiProperty({
+    description: 'user password',
+    minimum: 240,
+    default: '',
+  })
   password: string;
+
+  @ApiProperty({
+    description: 'user organization',
+    minimum: 240,
+    default: '',
+  })
   organization?: string;
 }
